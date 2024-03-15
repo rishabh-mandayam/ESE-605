bb0f7462fb16b4efebf4a4a20750ade62e6d4318
rishabh.mandayam@gmail.com <141846398+rishabh-mandayam@users.noreply.github.com>
2024-03-15 16:10:37 -0400

    Update q1.py

--- hw3/q1.py
+++ hw3/q1.py
@@ -1,23 +1,23 @@
 import cvxpy as cp
 import numpy as np
 
- #Generate a random feasible SOCP.
- m = 3
- n = 10
- p = 5
- n_i = 5
- np.random.seed(2)
- f = np.random.randn(n)
- A = []
- b = []
- c = []
- d = []
- x0 = np.random.randn(n)
- for i in range(m):
-     A.append(np.random.randn(n_i, n))
-     b.append(np.random.randn(n_i))
-     c.append(np.random.randn(n))
-     d.append(np.linalg.norm(A[i] @ x0 + b, 2) - c[i].T @ x0)
+#Generate a random feasible SOCP.
+m = 3
+n = 10
+p = 5
+n_i = 5
+np.random.seed(2)
+f = np.random.randn(n)
+A = []
+b = []
+c = []
+d = []
+x0 = np.random.randn(n)
+for i in range(m):
+    A.append(np.random.randn(n_i, n))
+    b.append(np.random.randn(n_i))
+    c.append(np.random.randn(n))
+    d.append(np.linalg.norm(A[i] @ x0 + b, 2) - c[i].T @ x0)
 
 F = np.random.randn(p, n)
 g = F @ x0
@@ -25,14 +25,12 @@ g = F @ x0
 x = cp.Variable(n)
 # We use cp.SOC(t, x) to create the SOC constraint ||x||_2 <= t.
 soc_constraints = [
-          cp.SOC(c[i].T @ x + d[i], A[i] @ x + b[i]) for i in range(m)
-
+    cp.SOC(c[i].T @ x + d[i], A[i] @ x + b[i]) for i in range(m)
 ]
-prob = cp.Problem(cp.Minimize(f.T@x),
-                                    soc_constraints + [F @ x == g])
+prob = cp.Problem(cp.Minimize(f.T@x),soc_constraints + [F @ x == g])
 prob.solve()
 
-# Print result.
+# Print result
 print("The optimal value is", prob.value)
 print("A solution x is")
 print(x.value)
